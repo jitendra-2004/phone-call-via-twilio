9fe9e2357069e69842acd71b486368da53c1ea8f
jitendra <jb303302@gmail.com>
2025-07-08 21:39:37 +0530

    Updated code or fixed bug

---
+++ email_module.py
@@ -0,0 +1,23 @@
+import smtplib
+
+def send_email():
+    sender_email = "enter your email"
+    app_password = "enter your app id"
+    receiver_email = input("Enter receiver email: ")
+    subject = input("Enter subject: ")
+    message = input("Enter message: ")
+
+    email_body = f"Subject: {subject}\n\n{message}"
+
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(sender_email, app_password)
+        server.sendmail(sender_email, receiver_email, email_body)
+        server.quit()
+        print(f"Email sent to {receiver_email}")
+    except Exception as e:
+        print("Failed to send email:", e)
+
+if __name__ == "__main__":
+    send_email()
